93fb98cf362d68fa99925f7afb16155b4f63c19c
Pat75013 <karukera92@gmail.com>
2024-06-04 14:17:50 +0200

    Ecran jeux avancement

--- ludik/ContentView.swift
+++ ludik/ContentView.swift
@@ -12,11 +12,56 @@ struct ContentView: View {
     
     
     var body: some View {
-        VStack {
-        Text("Quiz des Lettres")
-            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+        ZStack {
+            LinearGradient(colors:[.orange, .red], startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            VStack {
+            Text("Quiz des Lettres")
+                .font(.custom("Ranchers-Regular", size: 40))
+                .padding(.bottom)
+                HStack {
+                    VStack {
+                        Text("Mots: 1/20")
+                        Text("Score:10")
+                    }
+                    VStack {
+                        Text("0:00")
+                        Button {
+                            print("pause")
+                        } label: {
+                            Text("II")
+                                .foregroundStyle(.black)
+                        }
+                        
+                    }
+                }
+                .font(.custom("Ranchers-Regular", size: 25))
+                Image("Hippopotamus")
+                    .resizable()
+                    .frame(width: 130, height: 130, alignment: .center)
+                Text("Comment s'écrit ce mot ?")
+                    .font(.custom("Ranchers-Regular", size: 30))
+                
+                VStack {
+                    Text("Hipopotame")
+                        .bold()
+                        .frame(width: 280,height: 60)
+                        .background(.orange)
+                    .cornerRadius(20)
+                    Text("Hippopotame")
+                        .bold()
+                        .frame(width: 280,height: 60)
+                        .background(.orange)
+                        .cornerRadius(20)
+                    Text("Ippopotame")
+                        .bold()
+                        .frame(width: 280,height: 60)
+                        .background(.orange)
+                        .cornerRadius(20)
+                }
+                .font(.custom("Ranchers-Regular", size: 40))
+                .foregroundColor(.white)
+            }
         }
-        
     }
 }
 
