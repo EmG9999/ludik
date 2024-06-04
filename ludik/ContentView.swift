3a410b47ee3f5c5bd22c1a4e5542db0bcfbaf4f3
Dembod <diaouned@yahoo.fr>
2024-06-04 16:11:37 +0200

    jeux dembo

--- ludik/ContentView.swift
+++ ludik/ContentView.swift
@@ -9,20 +9,94 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
+    
+    
     var body: some View {
         
-        
-        HStack{
-            Text("hhh")
-                .font(.system(size: 36))
-                .bold()
+        ZStack{
+            LinearGradient(colors:[.black, .blue], startPoint: .top, endPoint: .center)
+                .edgesIgnoringSafeArea(.all)
+            
+            
+            
+            VStack {//Grande VStak Début
+                
+                    Text("Invasion des mots")
+                        .font(.custom("Ranchers-Regular", size: 30))
+                        .foregroundStyle(.green)
+                        .font(.title)
+                        .bold()
+                    
+                    
+                    
+                HStack{
+                    Text("manger ")
+                        .font(.custom("Ranchers-Regular", size: 20))
+                        .foregroundStyle(.white)
+                        .font(.title)
+                        .bold()
+                    
+                    
+                        
+                        Text("manger ")
+                            .font(.custom("Ranchers-Regular", size: 20))
+                            .foregroundStyle(.white)
+                            .font(.title)
+                            .bold()
+                            .padding(.top, 100)
+                        
+                        
+                    
+                    Text("manger ")
+                        .font(.custom("Ranchers-Regular", size: 20))
+                        .foregroundStyle(.white)
+                        .font(.title)
+                        .bold()
+                    
+                }
+                
+                
+                Spacer()
+                
+                ZStack{
+                    
+                    RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                        .frame(width: 300, height: 100)
+                    
+                    VStack{
+                        Text("Commencer")
+                            .foregroundStyle(.green)
+                            .font(.title)
+                            .bold()
+                            //.padding -> fait le padding et le margin
+                    }
+                    
+                    
+                }
+                
+                Spacer()
+                
+                VStack{
+                    Image("veseau")
+                        .resizable()
+                        .frame(width: 30,height: 30)
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 100)
+                            .frame(width: 50, height: 50)
+                        
+                        Image("house")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                    }
+                        
+                }
+                
+                
+            } //Grande VStack Fin
             
         }
-            
-        
-        
     }
-    }
+}
 #Preview {
     ContentView()
         .modelContainer(for: Item.self, inMemory: true)
