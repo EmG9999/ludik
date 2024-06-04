11a5f4a0f342557647927b4d090fd93c41061b73
sks667 <lahlouh.sophian@gmail.com>
2024-06-04 09:51:46 +0200

    gg

--- ludik/ContentView.swift
+++ ludik/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     
     var body: some View {
-       Text("je veux jouer a palworldddd")
+       Text("je veux jouer a paaaalworld")
             .bold()
         
     }
