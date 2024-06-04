a1dcbba9c2a2e3a6e30ac4cdbe2a629feae063ed
sks667 <lahlouh.sophian@gmail.com>
2024-06-04 14:14:28 +0200

    New jeu soso

--- ludik/ContentView.swift
+++ ludik/ContentView.swift
@@ -11,9 +11,42 @@ import SwiftData
 struct ContentView: View {
     
     
+    
     var body: some View {
-       Text("je veux jouerrrttttt ")
-            .bold()
+        let bubbleGumPink = Color(red: 0.3, green: 0.7, blue: 0.9, opacity: 0.5)
+        
+        
+        ZStack{
+            bubbleGumPink
+                .ignoresSafeArea()
+            RoundedRectangle(cornerSize: CGSize(width: 80, height: 30))
+            
+                .frame(width: 300, height: 100)
+            
+                
+                
+            Text("COMMENCER ")
+                .bold()
+                .font(.custom("Ranchers-Regular", size: 30))
+                .foregroundStyle(LinearGradient(
+                        colors: [.pink, .yellow],
+                        startPoint: .leading,
+                        endPoint: .trailing))
+                
+            
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
     }
 }
