bba2b21473381e253ccd94324fa0eb61a6e777b5
Pat75013 <karukera92@gmail.com>
2024-06-04 16:07:38 +0200

    Avancement ecran Pat jeux

--- ludik/ContentView.swift
+++ ludik/ContentView.swift
@@ -8,10 +8,18 @@
 import SwiftUI
 import SwiftData
 
+extension Color{
+    static let buttonChoiceOrange = Color(red: 236/255, green: 144/255, blue: 5/255)
+    static let buttonChoiceRed = Color(red: 228/255, green: 75/255, blue: 84/255)
+
+
+}
+    
+
 struct ContentView: View {
     
-    
     var body: some View {
+        
         ZStack {
             LinearGradient(colors:[.orange, .red], startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
             VStack {
@@ -31,40 +39,55 @@ struct ContentView: View {
                             Text("II")
                                 .foregroundStyle(.black)
                         }
-                        
                     }
                 }
                 .font(.custom("Ranchers-Regular", size: 25))
                 Image("Hippopotamus")
                     .resizable()
                     .frame(width: 130, height: 130, alignment: .center)
+                // Titre des 3 bouttons
                 Text("Comment s'écrit ce mot ?")
-                    .font(.custom("Ranchers-Regular", size: 30))
-                
+                    .font(.custom("Ranchers-Regular", size: 25))
+                // 3 bouttons de la sélectinn de la bonne réponse
                 VStack {
                     Text("Hipopotame")
-                        .bold()
-                        .frame(width: 280,height: 60)
+                        .frame(width: 267,height: 74)
                         .background(.orange)
-                    .cornerRadius(20)
+                        .cornerRadius(20)
                     Text("Hippopotame")
-                        .bold()
-                        .frame(width: 280,height: 60)
+                        .frame(width: 267,height: 74)
                         .background(.orange)
                         .cornerRadius(20)
                     Text("Ippopotame")
-                        .bold()
-                        .frame(width: 280,height: 60)
-                        .background(.orange)
+                        .frame(width: 267,height: 74)
+                        .background(LinearGradient(
+                                colors: [.buttonChoiceOrange, .buttonChoiceRed],
+                                startPoint: .leading,
+                                endPoint: .trailing))
                         .cornerRadius(20)
+                        
+                        
                 }
+                // Boutton "Commencer"
                 .font(.custom("Ranchers-Regular", size: 40))
                 .foregroundColor(.white)
+                .padding(/*@START_MENU_TOKEN@*/EdgeInsets()/*@END_MENU_TOKEN@*/)
+                
+                
+                Text("Commencer")
+                    .frame(width: 327,height: 48)
+                    .background(.orange)
+                    .cornerRadius(80)
+                    .foregroundColor(.white)
+                    .font(.custom("Ranchers-Regular", size: 30))
             }
+            
+            
+            
         }
+        
     }
 }
-
 #Preview {
     ContentView()
         .modelContainer(for: Item.self, inMemory: true)
