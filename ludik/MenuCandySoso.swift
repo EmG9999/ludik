a3f13eb98c498e145618425ad35c2292cf7ff61b
sks667 <lahlouh.sophian@gmail.com>
2024-06-06 11:03:27 +0200

    New menu

--- ludik/MenuCandySoso.swift
+++ ludik/MenuCandySoso.swift
@@ -35,42 +35,124 @@ struct MenuCandySoso: View {
                             .resizable()
                             .frame(width: 400.0, height: 250.0)
                             .ignoresSafeArea()
-                Text("CANDY CRUNCH")
+                VStack{
+                    Image("Vector")
+                        .padding(.bottom, 60)
+                    Text("CANDY CRUNCH")
                         .font(.custom("Ranchers-Regular", size: 60))
                         .foregroundStyle(
-                                LinearGradient(gradient: Gradient(colors: [.bubbleGumPink, .bubbleGumYellow]), startPoint: .topTrailing, endPoint: .bottom))
-                        
-                        
+                            LinearGradient(gradient: Gradient(colors: [.bubbleGumPink, .bubbleGumYellow]), startPoint: .topTrailing, endPoint: .bottom))
+                    
+                }
                     }
         Spacer()
+            
+            HStack{
+                VStack{
+                    Image("Boule1")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule10")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule7")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule8")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                }.padding(.horizontal, 2)
                 
+                
+                VStack{
+                    Image("Boule6")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule10")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule10")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule1")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                }.padding(.horizontal)
+                
+                
+                VStack{
+                    Image("Boule4")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule9")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule7")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule4")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                }.padding(.horizontal, 2)
+                
+                
+                VStack{
+                    Image("Boule6")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule10")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule1")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule3")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                }.padding(.horizontal, 10)
+                
+                
+            }
+            
                 ZStack{
+                    
                     RoundedRectangle(cornerSize: CGSize(width: 80, height: 30))
                         .fill(Color.buttonBlue)
                         .frame(width: 300, height: 120)
                 
-                    Text("COMMENCERr ")
+                    Text("COMMENCER")
                         .font(.custom("Ranchers-Regular", size: 40))
                         .foregroundStyle(
                                 LinearGradient(gradient: Gradient(colors: [.bubbleGumPink, .bubbleGumYellow]), startPoint: .topTrailing, endPoint: .bottom))
                     
                         
                         
-                }
-                    
-                    
-                    Spacer()
-                    Circle()
-                        .frame(width: 90)
-                        .foregroundStyle(
-                                LinearGradient(gradient: Gradient(colors: [.bubbleGumPink, .backBlue2]), startPoint: .topTrailing, endPoint: .bottom))
-                    
-                    
+                }.padding(.bottom, 30)
+            
+     
+            Spacer()
+            Spacer()
 
+            
+            
+            
+            VStack{
+                ZStack{
+                    Circle()
+                        .frame(width: 700, height: 70)
+                        .foregroundStyle(
+                            LinearGradient(gradient: Gradient(colors: [.bubbleGumPink, .backBlue2]), startPoint: .topTrailing, endPoint: .bottom))
                     
-                    
-                    
-                    
+                    Image("house")
+                        .resizable()
+                        .frame(width: 50, height: 45)
+
+                }
+                
+               Image("Vector2")
+                
+                
+            }
             }
         }
     }
@@ -93,3 +175,4 @@ struct MenuCandySoso: View {
 #Preview {
     MenuCandySoso()
 }
+
--- ludik/candyCrunchMenu.swift
+++ ludik/candyCrunchMenu.swift
@@ -15,7 +15,7 @@ extension Color {
     static let buttonBlue = Color(red: 111/255, green: 157/255, blue: 181/255)
 }
 
-struct candyCrunchMenu: View {
+struct MenuCandySoso: View {
     var body: some View {
         
         
@@ -35,42 +35,124 @@ struct candyCrunchMenu: View {
                             .resizable()
                             .frame(width: 400.0, height: 250.0)
                             .ignoresSafeArea()
-                Text("CANDY CRUNCH")
+                VStack{
+                    Image("Vector")
+                        .padding(.bottom, 60)
+                    Text("CANDY CRUNCH")
                         .font(.custom("Ranchers-Regular", size: 60))
                         .foregroundStyle(
-                                LinearGradient(gradient: Gradient(colors: [.bubbleGumPink, .bubbleGumYellow]), startPoint: .topTrailing, endPoint: .bottom)) 
-                        
-                        
+                            LinearGradient(gradient: Gradient(colors: [.bubbleGumPink, .bubbleGumYellow]), startPoint: .topTrailing, endPoint: .bottom))
+                    
+                }
                     }
         Spacer()
+            
+            HStack{
+                VStack{
+                    Image("Boule1")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule10")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule7")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule8")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                }.padding(.horizontal, 2)
                 
+                
+                VStack{
+                    Image("Boule6")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule10")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule10")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule1")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                }.padding(.horizontal)
+                
+                
+                VStack{
+                    Image("Boule4")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule9")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule7")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule4")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                }.padding(.horizontal, 2)
+                
+                
+                VStack{
+                    Image("Boule6")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule10")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule1")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                    Image("Boule3")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                }.padding(.horizontal, 10)
+                
+                
+            }
+            
                 ZStack{
+                    
                     RoundedRectangle(cornerSize: CGSize(width: 80, height: 30))
                         .fill(Color.buttonBlue)
                         .frame(width: 300, height: 120)
                 
-                    Text("COMMENCERr ")
+                    Text("COMMENCER")
                         .font(.custom("Ranchers-Regular", size: 40))
                         .foregroundStyle(
                                 LinearGradient(gradient: Gradient(colors: [.bubbleGumPink, .bubbleGumYellow]), startPoint: .topTrailing, endPoint: .bottom))
                     
                         
                         
-                }
-                    
-                    
-                    Spacer()
-                    Circle()
-                        .frame(width: 90)
-                        .foregroundStyle(
-                                LinearGradient(gradient: Gradient(colors: [.bubbleGumPink, .backBlue2]), startPoint: .topTrailing, endPoint: .bottom))
-                    
-                    
+                }.padding(.bottom, 30)
+            
+     
+            Spacer()
+            Spacer()
 
+            
+            
+            
+            VStack{
+                ZStack{
+                    Circle()
+                        .frame(width: 700, height: 70)
+                        .foregroundStyle(
+                            LinearGradient(gradient: Gradient(colors: [.bubbleGumPink, .backBlue2]), startPoint: .topTrailing, endPoint: .bottom))
                     
-                    
-                    
-                    
+                    Image("house")
+                        .resizable()
+                        .frame(width: 50, height: 45)
+
+                }
+                
+               Image("Vector2")
+                
+                
+            }
             }
         }
     }
@@ -91,5 +173,5 @@ struct candyCrunchMenu: View {
 
 
 #Preview {
-    candyCrunchMenu()
+    MenuCandySoso()
 }
