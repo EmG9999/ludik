a083beb318d1f22831e6eb5b6e9f3ac6fadcc2a3
EmG9999 <emilianomondorge@gmail.Com>
2024-06-07 17:23:18 +0200

    ecran profil

--- ludik/profilMain.swift
+++ ludik/profilMain.swift
@@ -7,12 +7,62 @@
 
 import SwiftUI
 
-struct profilMain: View {
+struct profil: View {
+    
+    private let startColor: Color = .orange
+    private let endColor: Color = .red
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+        LinearGradient(colors: [startColor, endColor], startPoint: .center, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.all)
+            VStack {
+                
+                Text("DEMPASO")
+                    .font(.custom("Ranchers-Regular", fixedSize: 30))
+                ZStack {
+                    
+                    Circle()
+                        .frame(height: 250)
+                    Image("GABI 1")
+                }
+    
+        Text("LVL 35")
+                    .font(.custom("Ranchers", fixedSize: 30))
+                    
+                
+                
+                ZStack {
+              RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                    .frame(width: 370, height: 30)
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                        .position(x: 115, y: 15)
+                        .frame(width:300, height: 30)
+                        .foregroundStyle(.yellow)
+                }
+                Spacer()
+                VStack {
+                    HStack {
+                        Image("Badge_01")
+                        Image("Badge_02")
+                        Image("Badge_03")
+                        Image("Badge_04")
+                    }
+                    HStack {
+                        Image("Badge_05")
+                        Image("Badge_06")
+                        Image("Badge_07")
+                        Image("Badge_08")
+                    }
+                    Image("Badge_09")
+                }
+                
+            }
+        }
     }
 }
 
 #Preview {
-    profilMain()
+    profil()
 }
