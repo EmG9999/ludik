33a30642532569cd2e045fc026c95a539c391bc8
EmG9999 <emilianomondorge@gmail.Com>
2024-06-07 17:20:03 +0200

    ecran profil

---
+++ ludik/profilMain.swift
@@ -0,0 +1,18 @@
+//
+//  profilMain.swift
+//  ludik
+//
+//  Created by Apprenant 165 on 07/06/2024.
+//
+
+import SwiftUI
+
+struct profilMain: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    profilMain()
+}
