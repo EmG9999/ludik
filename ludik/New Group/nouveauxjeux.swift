7f9cbc1bf062e11c0c7c254d0d1f92a9f464db2a
EmG9999 <emilianomondorge@gmail.Com>
2024-06-27 14:15:45 +0200

    ajouts des structures sur l'ecran profil

--- ludik/New Group/nouveauxjeux.swift
+++ ludik/New Group/nouveauxjeux.swift
@@ -7,95 +7,99 @@
 
 import SwiftUI
 
-struct nouveauxjeux: View {
-    @State private var correctAnswer = 0
-       @State private var choiceArray : [Int] = [0, 1, 2, 3]
-       @State private var firstNumber = 0
-       @State private var secondNumber = 0
-       @State private var difficulté = 100
-       @State private var score = 0
-    private let startColor: Color = .orange
-    private let endColor: Color = .red
+    struct nouveauxjeux: View {
+        
        
-       var body: some View {
-           
-           ZStack {
-               LinearGradient(colors: [startColor, endColor], startPoint: .center, endPoint: .bottom)
-                   .edgesIgnoringSafeArea(.all)
-               
-               VStack {
-                   Text("\(firstNumber) + \(secondNumber)")
-                       .font(.custom("Ranchers", size: 40))
-                     
-                   
-                   HStack {
-                       ForEach(0..<2) {index in
-                           Button {
-                               answerIsCorrect(answer: choiceArray[index])
-                               generateAnswers()
-                           } label: {
-                               AnswerButton(number: choiceArray[index])
-                           }
-                       }
-                   }
-                   
-                   HStack {
-                       ForEach(2..<4) {index in
-                           Button {
-                               answerIsCorrect(answer: choiceArray[index])
-                               generateAnswers()
-                           } label: {
-                               AnswerButton(number: choiceArray[index])
-                           }
-                       }
-                   }
-                   Text("Score: \(score)")
-                       .font(.headline)
-                       .bold()
-               }.onAppear(perform: generateAnswers)
-           }
-           
-       }
-       
-       func answerIsCorrect(answer: Int){
-           if answer == correctAnswer {
-               score += 1
-           } else {
-               score -= 1
-           }
-           if score > 10 {
-               difficulté += 100 // la diff s'incremente a chaque serie de 10 rep
-           }
-           if score > 20 {
-               difficulté += 800
-           }
-           if score > 20 {
-               difficulté += 9000
-           }
-       }
-       
-       
-       func generateAnswers(){
-           firstNumber = Int.random(in: 0...(difficulté/2))
-           secondNumber = Int.random(in: 0...(difficulté/2))
-           var answerList = [Int]() // genere une liste vide des reponses possibles
-           
-           correctAnswer = firstNumber + secondNumber
-           
-           for _ in 0...2 {
-               answerList.append(Int.random(in: 0...difficulté))
-           }
-           
-           answerList.append(correctAnswer) // met la bonne reponses dans la liste
-           
-           choiceArray = answerList.shuffled() // met la bonne reponses dans un endroit aleatoire
-       }
-   
-   }
-
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        nouveauxjeux()
+        @State private var correctAnswer = 0
+        @State private var choiceArray : [Int] = [0, 1, 2, 3]
+        @State private var firstNumber = 0
+        @State private var secondNumber = 0
+        @State private var difficulté = 100
+        @State private var score = 0
+        private let startColor: Color = .orange
+        private let endColor: Color = .red
+        
+        var body: some View {
+            
+            ZStack {
+                LinearGradient(colors: [startColor, endColor], startPoint: .center, endPoint: .bottom)
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    Text("\(firstNumber) + \(secondNumber)")
+                        .font(.custom("Ranchers", size: 40))
+                    
+                    
+                    HStack {
+                        ForEach(0..<2) {index in
+                            Button {
+                                answerIsCorrect(answer: choiceArray[index])
+                                generateAnswers()
+                            } label: {
+                                AnswerButton(number: choiceArray[index])
+                            }
+                        }
+                    }
+                    
+                    HStack {
+                        ForEach(2..<4) {index in
+                            Button {
+                                answerIsCorrect(answer: choiceArray[index])
+                                generateAnswers()
+                            } label: {
+                                AnswerButton(number: choiceArray[index])
+                            }
+                        }
+                    }
+                    Text("Score: \(score)")
+                        .font(.headline)
+                        .bold()
+                }.onAppear(perform: generateAnswers)
+            }
+            
+        }
+        
+        func answerIsCorrect(answer: Int){
+            if answer == correctAnswer {
+                score += 1
+            } else {
+                score -= 1
+            }
+            if score == 10 {
+                difficulté += 100 // la diff s'incremente a chaque serie de 10 rep
+            }
+            if score == 20 {
+                difficulté += 800
+            }
+            if score == 30 {
+                difficulté += 9000
+            }
+    
+        }
+        
+        
+        func generateAnswers(){
+            firstNumber = Int.random(in: 0...(difficulté/2))
+            secondNumber = Int.random(in: 0...(difficulté/2))
+            var answerList = [Int]() // genere une liste vide des reponses possibles
+            
+            correctAnswer = firstNumber + secondNumber
+            
+            for _ in 0...2 {
+                answerList.append(Int.random(in: 0...difficulté))
+            }
+            
+            answerList.append(correctAnswer) // met la bonne reponses dans la liste
+            
+            choiceArray = answerList.shuffled() // met la bonne reponses dans un endroit aleatoire
+        }
+        
     }
-}
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            nouveauxjeux()
+        }
+    }
+
--- ludik/profil.swift
+++ ludik/profil.swift
@@ -6,9 +6,25 @@
 //
 
 import SwiftUI
-
+struct profile {
+    var pseudo: String
+    var niveau: Int
+    var avatar: String
+    var badge: badges
+}
+struct badges {
+    var badge1 = "Badge_01"
+    var badge2 = "Badge_02"
+    var badge3 = "Badge_03"
+    var badge4 = "Badge_04"
+    var badge5 = "Badge_05"
+    var badge6 = "Badge_06"
+    var badge7 = "Badge_07"
+    var badge8 = "Badge_08"
+    var badge9 = "Badge_09"
+}
 struct profil: View {
-    
+    let user = profile(pseudo: "DEMPASO", niveau: 35, avatar: "GABI 1", badge: badges())
     private let startColor: Color = .orange
     private let endColor: Color = .red
     
@@ -19,16 +35,18 @@ struct profil: View {
             .edgesIgnoringSafeArea(.all)
             VStack {
                 
-                Text("DEMPASO")
+                Text(user.pseudo)
                     .font(.custom("Ranchers-Regular", fixedSize: 30))
+                    
                 ZStack {
                     
                     Circle()
                         .frame(height: 250)
-                    Image("GABI 1")
+                        .foregroundStyle(.white)
+                    Image(user.avatar)
                 }
     
-        Text("LVL 35")
+                Text("LVL \(user.niveau)")
                     .font(.custom("Ranchers", fixedSize: 30))
                     
                 
@@ -36,6 +54,7 @@ struct profil: View {
                 ZStack {
               RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
                     .frame(width: 370, height: 30)
+                    .foregroundStyle(.white)
                 RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
                         .position(x: 115, y: 15)
                         .frame(width:300, height: 30)
@@ -44,20 +63,19 @@ struct profil: View {
                 Spacer()
                 VStack {
                     HStack {
-                        Image("Badge_01")
-                        Image("Badge_02")
-                        Image("Badge_03")
-                        Image("Badge_04")
+                        Image(user.badge.badge1)
+                        Image(user.badge.badge2)
+                        Image(user.badge.badge3)
+                        Image(user.badge.badge4)
                     }
                     HStack {
-                        Image("Badge_05")
-                        Image("Badge_06")
-                        Image("Badge_07")
-                        Image("Badge_08")
+                        Image(user.badge.badge5)
+                        Image(user.badge.badge6)
+                        Image(user.badge.badge7)
+                        Image(user.badge.badge8)
                     }
-                    Image("Badge_09")
+                    Image(user.badge.badge9)
                 }
-                
             }
         }
     }
