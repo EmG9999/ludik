d71493af076a78a6c1fc4301f613abe759f145cc
Dembod <diaouned@yahoo.fr>
2024-06-28 10:55:52 +0200

    struct dembo

---
+++ ludik/StructDembo.swift
@@ -0,0 +1,18 @@
+//
+//  StructDembo.swift
+//  ludik
+//
+//  Created by Apprenant 142 on 28/06/2024.
+//
+
+import SwiftUI
+
+struct StructDembo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StructDembo()
+}
