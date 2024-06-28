e85a66160d1ad7c28ad41f6769e8a8d129809c97
Pat75013 <karukera92@gmail.com>
2024-06-28 10:31:37 +0200

    ajout Words View

---
+++ ludik/Word.swift
@@ -0,0 +1,18 @@
+//
+//  Word.swift
+//  ludik
+//
+//  Created by Apprenant 123 on 28/06/2024.
+//
+
+import SwiftUI
+
+struct Word: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Word()
+}
